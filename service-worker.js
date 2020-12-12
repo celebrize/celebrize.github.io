b04c6fd18741a05ac862dbe7d64a8bcc2bc04a29
czenker <czenker@users.noreply.github.com>
2020-12-12 20:59:22 +0000

    deploy: 4ebfd63282870ebd50e5a01ced867327444060c4

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anniversaries/precache-manifest.b4f43ec864e91593a25c848ccf159b7b.js"
+  "/precache-manifest.123656153cbdc4ed12592c9b697c1585.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anniversaries/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
