f7d55260c91d04147f1f45a8600ed13644a5fde5
Christian Zenker <dev@xopn.de>
2020-12-12 20:49:25 +0100

    move to new domain

--- src/static.js
+++ src/static.js
@@ -2,7 +2,7 @@
 const appName = "Celebrize"
 const appTagLine = "a reason to celebrate"
 const appShareText = "Check this out"
-const githubUri = "https://github.com/czenker/anniversaries"
+const githubUri = "https://github.com/celebrize/celebrize.github.io"
 const githubIssuesUri = githubUri + "/issues"
 const githubPullsUri = githubUri + "/pulls"
 const xopnUri = "http://xopn.de"
